def39ac90fc79cfff13c9e64111bb3dc3de32faa
priyanshu-sharma <prynsh1112@gmail.com>
2023-12-09 19:02:24 -0800

    added vector to image data

--- starter.cu
+++ starter.cu
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <vector>
 using namespace cv;
 using namespace std;
 
@@ -18,17 +19,20 @@ int main(int argc, char* argv[])
     int width = image.cols;
     int height = image.rows;
     int _stride = image.step;
-    int count = 0;
+    // int count = 0;
+    vector<int> image_vector;
     for(int i = 0; i < height; i++)
     {
         for(int j = 0; j < width; j++)
         {
             uint8_t val = myData[ i * _stride + j];
-            cout<<"Image at : "<<i<<" , "<<j<<" - "<<unsigned(val)<<endl;
-            count = count + 1;
+            // cout<<"Image at : "<<i<<" , "<<j<<" - "<<unsigned(val)<<endl;
+            // count = count + 1;
+            image_vector.push_back(unsigned(val));
         }
     }
     cout<<"Count="<<count<<endl;
+    cout<<"Size="<<image_vector.size()<<endl;
     cout<<"Width="<<unsigned(width)<<endl;
     cout<<"Height="<<unsigned(height)<<endl;
     cout<<"Stride="<<unsigned(_stride)<<endl;
