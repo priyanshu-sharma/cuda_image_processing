1c85fe32a62c7b093f44049e46a0501f52845995
priyanshu-sharma <prynsh1112@gmail.com>
2023-12-09 19:42:09 -0800

    added gpu component

--- starter.cu
+++ starter.cu
@@ -6,35 +6,74 @@
 using namespace cv;
 using namespace std;
 
+# define BLOCK_SIZE 512
+
+
+__global__ void scaling_kernel(unsigned int* input, unsigned int size)
+{
+	
+    /*************************************************************************/
+    // INSERT KERNEL CODE HERE
+    int i = threadIdx.x + blockDim.x * blockIdx.x;
+    if (i < size)
+    {
+        input[i] = 2 * input[i]
+    }
+	/*************************************************************************/
+}
+
+
+void scaling(unsigned int* input, unsigned int size) {
+
+	  /*************************************************************************/
+    //INSERT CODE HERE
+    dim3 DimGrid((size - 1)/BLOCK_SIZE + 1, 1, 1);
+    dim3 DimBlock(BLOCK_SIZE, 1, 1);
+
+    scaling_kernel<<<DimGrid, DimBlock>>>(input, size);
+	  /*************************************************************************/
+}
+
 int main(int argc, char* argv[])
 {
     Mat image = imread("demo.png", IMREAD_GRAYSCALE);
     if (!image.data) { 
         printf("No image data \n");  
     }
-    else{
-        printf("data\n");
-    }
     uint8_t *myData = image.data;
     int width = image.cols;
     int height = image.rows;
     int _stride = image.step;
-    // int count = 0;
-    vector<int> image_vector;
+    unsigned int size = width * height * sizeof(unsigned int);
+    cout<<"Size="<<size<<endl;
+    cout<<"Width="<<unsigned(width)<<endl;
+    cout<<"Height="<<unsigned(height)<<endl;
+    cout<<"Stride="<<unsigned(_stride)<<endl;
+    unsigned int *image_vector = (unsigned int *) malloc(size);
     for(int i = 0; i < height; i++)
     {
         for(int j = 0; j < width; j++)
         {
             uint8_t val = myData[ i * _stride + j];
-            // cout<<"Image at : "<<i<<" , "<<j<<" - "<<unsigned(val)<<endl;
-            // count = count + 1;
-            image_vector.push_back(unsigned(val));
+            image_vector[i * _stride + j] = unsigned(val);
         }
     }
-    // cout<<"Count="<<count<<endl;
-    cout<<"Size="<<image_vector.size()<<endl;
-    cout<<"Width="<<unsigned(width)<<endl;
-    cout<<"Height="<<unsigned(height)<<endl;
-    cout<<"Stride="<<unsigned(_stride)<<endl;
+    unsigned int *image_vector_d;
+    cudaMalloc((void **) &image_vector_d, size);
+    unsigned int *o_image_vector = (unsigned int *) malloc(size);
+    cudaMemcpy(image_vector_d, image_vector, size, cudaMemcpyHostToDevice);
+    cudaDeviceSynchronize();
+    startTime(&timer);
+    scaling(image_vector_d, size);
+    cuda_ret = cudaDeviceSynchronize();
+    if(cuda_ret != cudaSuccess) printf("Unable to launch kernel");
+    stopTime(&timer); printf("%f s\n", elapsedTime(timer));
+
+    // Copy device variables from host ----------------------------------------
+    printf("Copying data from device to host..."); fflush(stdout);
+    cudaMemcpy(o_image_vector, image_vector_d, size, cudaMemcpyDeviceToHost);
+    cout<<o_image_vector[262143]<<" - "<<image_vector[262143]<<endl;
+    free(image_vector);
+    cudaFree(o_image_vector);
     return 0;
 }
