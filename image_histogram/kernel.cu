9ef41725f39e1bcd68869f9d5215fedf8f461db5
priyanshu-sharma <prynsh1112@gmail.com>
2023-12-11 03:11:53 -0800

    hf error

--- image_histogram/kernel.cu
+++ image_histogram/kernel.cu
@@ -49,7 +49,7 @@ __global__ void image_histogram_kernel(double* input, int size, double* histogra
     stride = blockDim.x * gridDim.x;
     while ( i < size )
     {
-        equalized_value = input[i];
+        int equalized_value = input[i];
         final_output[i] = cdf[equalized_value];
         i += stride;
     }
